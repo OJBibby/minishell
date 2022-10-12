fc4dded1ae2037b872733da3fd397ee3e0cf8221
obibby <obibby@student.42wolfsburg.de>
2022-10-12 14:08:39 +0100

    fix prompt after signal

--- signals/signals.c
+++ signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: obibby <obibby@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 11:51:20 by obibby            #+#    #+#             */
-/*   Updated: 2022/10/12 13:28:36 by obibby           ###   ########.fr       */
+/*   Updated: 2022/10/12 14:08:07 by obibby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,22 @@
 
 void	sig_quit(int sig)
 {
-	//rl_on_new_line();
-	//rl_redisplay();
 	g_status.exit_status = 128 + sig;
 }
 
 void	sig_int(int sig)
 {
 	if (g_status.pid != 0)
+	{
 		kill(g_status.pid, SIGINT);
-	write(1, "^C\n", 3);
-	rl_replace_line("", 1);
-	rl_on_new_line();
-	rl_redisplay();
+		write(1, "\n", 1);
+	}
+	else
+	{
+		rl_replace_line("", 1);
+		write(1, "^C\n", 3);
+		rl_on_new_line();
+		rl_redisplay();
+	}
 	g_status.exit_status = 128 + sig;
 }
